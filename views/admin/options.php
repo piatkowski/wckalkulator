7f03189bad375e0c830503b8736d8d99309b8f99
Krzysztof Piątkowski <support@wckalkulator.com>
2022-08-05 00:20:37 +0200

    Update label

--- views/admin/options.php
+++ views/admin/options.php
@@ -15,7 +15,7 @@ global $post;
     <input type="checkbox" name="_wck_variation_prices_visible" id="wck_variation_prices_visible"
            value="1" <?php echo checked($enabled); ?> />
     <label for="wck_variation_prices_visible">
-        <?php _e('Show variation prices', 'wc-kalkulator'); ?>
+        <?php _e("Show price blocks for variable products", 'wc-kalkulator'); ?>
         <?php echo Helper::html_help_tip(__('Check this option to show variation price blocks on product page. Default: hidden.', 'wc-kalkulator')); ?>
     </label>
 </p>
