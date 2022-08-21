73b060a4b0b3dbb618bd37faa39967fbded38e0f
Krzysztof Piątkowski <support@wckalkulator.com>
2022-08-21 22:44:06 +0200

    Performance tweaks, version tag update

--- src/FieldsetPostType.php
+++ src/FieldsetPostType.php
@@ -108,7 +108,7 @@ class FieldsetPostType
      */
     public static function register_post_type()
     {
-        if (post_type_exists(self::POST_TYPE)) {
+        if (!current_user_can('manage_woocommerce') || post_type_exists(self::POST_TYPE)) {
             return;
         }
 
